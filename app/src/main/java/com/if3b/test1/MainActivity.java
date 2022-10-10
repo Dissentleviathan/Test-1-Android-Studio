4fb60d14aa21e34e593ae305769bc6c9c975c35a
Dissentleviathan <maghasp22@gmail.com>
2022-10-10 11:54:37 +0700

    test 1

---
+++ app/src/main/java/com/if3b/test1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.if3b.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button btnPindah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Layout Satu");
+
+        btnPindah = findViewById(R.id.btn_pindah);
+
+        btnPindah.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                  Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(pindah);
+                }
+
+        });
+    }
+}
